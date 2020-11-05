c11737f910a647a390ce13eb64f78c682442ff28
darrenbawag <renash12@gmail.com>
2020-11-05 08:42:21 +0800

    Add dropdown after search bar on home page index.htm

--- js/script.js
+++ js/script.js
@@ -419,3 +419,27 @@ $(".rd-navbar-sl-close-toggle").on("click", function () {
 // $(document).ready(function () {
 //     $('#example').DataTable();
 // });
+
+// reCaptcha responsive
+
+$(function () {
+    var width = $('.g-recaptcha').parent().width();
+    if (width < 302) {
+        var scale = width / 302;
+        $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
+        $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
+        $('.g-recaptcha').css('transform-origin', '0 0');
+        $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
+    }
+});
+
+$(window).on("resize", function () {
+    var width = $('.g-recaptcha').parent().width();
+    if (width < 302) {
+        var scale = width / 302;
+        $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
+        $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
+        $('.g-recaptcha').css('transform-origin', '0 0');
+        $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
+    }
+});
